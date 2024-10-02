fc18542452f295c3441dd02c680fde51e9d9b883
anniesalis <anniesalis02@gmail.com>
2024-10-02 13:15:53 +0100

    Added styling to CSS

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 function displayJoke(response) {
   console.log(response.data.answer);
-
-  new Typewriter("#joke", {
-    strings: response.data.answer,
-    autoStart: true,
-    cursor: null,
-    delay: 20,
-  });
+  
+  
+new Typewriter("#joke", {
+  strings: response.data.answer,
+  autoStart: true,
+  cursor: null,
+  delay: 20,
+});
 }
 
 function generateJoke(event) {
   event.preventDefault();
 
   let apiKey = "2ob113a879d9f74f53b31fb0t04ab5cb";
-  let context = "Please be funny";
+  let context = "Please be funny and brief and go directly to the joke include laughter emojis";
   let prompt = "Tell me a funny joke in pidgin";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  let loadingElement = document.querySelector("#loading");
-  loadingElement.innerHTML = "loading....";
+  let jokeElement = document.querySelector("#loading");
+  jokeElement.innerHTML = "loading....";
 
   console.log("called the AI api");
   axios.get(apiUrl).then(displayJoke);
 }
-let buttonElement = document.querySelector("#joke");
+let buttonElement = document.querySelector("button");
 let loadingElement = document.querySelector("#loading");
 buttonElement.addEventListener("click", generateJoke);
